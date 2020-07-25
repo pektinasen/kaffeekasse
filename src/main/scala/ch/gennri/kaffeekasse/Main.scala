e9bee3c296f31ed8646a7f00892e8fdc596dd7f2
Saskia Gennrich <saskia.gennrich@posteo.de>
2020-07-25 11:35:43 +0200

    initial

---
+++ src/main/scala/ch/gennri/kaffeekasse/Main.scala
@@ -0,0 +1,9 @@
+package ch.gennri.kaffeekasse
+
+import cats.effect.{ExitCode, IO, IOApp}
+import cats.implicits._
+
+object Main extends IOApp {
+  def run(args: List[String]) =
+    KaffeekasseServer.stream[IO].compile.drain.as(ExitCode.Success)
+}
